deb50b7b5cb0f46ec7120fb286198b352efe56fb
syukri21 <syukrihsb148@gmail.com>
2024-04-18 22:26:52 +0700

    add base template

--- rchaty-server/src/page_handlres.rs
+++ rchaty-server/src/page_handlres.rs
@@ -3,15 +3,15 @@ use axum::{extract::Query, response::Html};
 use rchaty_web::ErrorTemplate;
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Clone, Deserialize, Serialize)]
 pub struct ErrorPageQueryParams {
-    pub msg :String
+    pub msg: String,
 }
 
-
-pub async fn error_page(Query(params): Query<ErrorPageQueryParams>) -> axum::response::Html<String> {
-    let template = ErrorTemplate { error: &params.msg};
+pub async fn error_page(
+    Query(params): Query<ErrorPageQueryParams>,
+) -> axum::response::Html<String> {
+    let template = ErrorTemplate { error: &params.msg };
     let html = template.render().unwrap();
     Html(html)
 }
